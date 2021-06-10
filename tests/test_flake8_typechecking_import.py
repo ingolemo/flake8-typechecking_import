8a754c0d3bd0a9af51b0fda649e9d2b625ede649
Adrian Room <ingolemo@gmail.com>
2021-06-11 00:30:12 +0100

    add type annotations on tests

--- tests/test_flake8_typechecking_import.py
+++ tests/test_flake8_typechecking_import.py
@@ -11,53 +11,53 @@ def _results(lines: List[str]) -> Set[str]:
     return {msg.split(" ")[0] for line, col, msg, _ in plugin.run()}
 
 
-def test_trivial_case():
-    assert _results("") == set()
+def test_trivial_case() -> None:
+    assert _results([""]) == set()
 
 
-def test_unused_import():
+def test_unused_import() -> None:
     lines = ["import mod"]
     assert _results(lines) == set()
     lines = ["import mod as alias"]
     assert _results(lines) == set()
 
 
-def test_used_import():
+def test_used_import() -> None:
     lines = ["import mod", "mod"]
     assert _results(lines) == set()
     lines = ["import mod as alias", "alias"]
     assert _results(lines) == set()
 
 
-def test_annotation():
+def test_annotation() -> None:
     lines = ["import mod", "var: mod"]
     assert _results(lines) == {"TCI100"}
     lines = ["import mod as alias", "var: alias"]
     assert _results(lines) == {"TCI100"}
 
 
-def test_annotated_assignment():
+def test_annotated_assignment() -> None:
     lines = ["import mod", "var: mod = []"]
     assert _results(lines) == {"TCI100"}
     lines = ["import mod as alias", "var: alias = []"]
     assert _results(lines) == {"TCI100"}
 
 
-def test_annotated_assignment_value():
+def test_annotated_assignment_value() -> None:
     lines = ["import mod", "var: type = mod"]
     assert _results(lines) == set()
     lines = ["import mod as alias", "var: type = alias"]
     assert _results(lines) == set()
 
 
-def test_multiple_unused_imports():
+def test_multiple_unused_imports() -> None:
     lines = ["import mod", "import pack"]
     assert _results(lines) == set()
     lines = ["import mod as mod_alias", "import pack as pack_alias"]
     assert _results(lines) == set()
 
 
-def test_function_arguments():
+def test_function_arguments() -> None:
     lines = ["def func(arg):", "    pass"]
     assert _results(lines) == set()
     lines = ["import mod", "def func(arg: pack):", "    pass"]
@@ -72,21 +72,21 @@ def test_function_arguments():
     assert _results(lines) == {"TCI100"}
 
 
-def test_package():
+def test_package() -> None:
     lines = ["import pack.mod"]
     assert _results(lines) == set()
     lines = ["import pack.mod", "pack"]
     assert _results(lines) == set()
 
 
-def test_unused_type_checking():
+def test_unused_type_checking() -> None:
     lines = ["import typing", "if typing.TYPE_CHECKING:", "    import mod"]
     assert _results(lines) == set()
     lines = ["from typing import TYPE_CHECKING", "if TYPE_CHECKING:", "    import mod"]
     assert _results(lines) == set()
 
 
-def test_annotated_type_checking():
+def test_annotated_type_checking() -> None:
     lines = ["import typing", "if typing.TYPE_CHECKING:", "    import mod", "var: mod"]
     assert _results(lines) == set()
     lines = [
@@ -98,12 +98,12 @@ def test_annotated_type_checking():
     assert _results(lines) == set()
 
 
-def test_non_type_checking_if():
+def test_non_type_checking_if() -> None:
     lines = ["if True:", "    var = 0"]
     assert _results(lines) == set()
 
 
-def test_shadowing():
+def test_shadowing() -> None:
     lines = ["import mod", "mod = 0", "var: mod.type = 0"]
     assert _results(lines) == set()
     # FIXME: handle shadowing of variables
@@ -111,12 +111,12 @@ def test_shadowing():
     # assert _results(lines) == {"TCI100"}
 
 
-def test_function_return():
+def test_function_return() -> None:
     lines = ["import mod", "def func() -> mod:", "    pass"]
     assert _results(lines) == {"TCI100"}
 
 
-def test_relative_imports():
+def test_relative_imports() -> None:
     lines = ["from . import mod"]
     assert _results(lines) == set()
     lines = ["from . import mod", "var = mod.value"]
